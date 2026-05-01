9f08560301944f1ba07d94d118f07d9cc74c8105
LazyBeaver007 <adiprime007@gmail.com>
2026-05-01 12:10:26 +0530

    Page systems with .vkp storage and loading + UI tweaks by codex

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,67 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 use std::fs::read_to_string;
 use std::path::Path;
+use uuid::{Uuid};
+
+
+
+
+#[tauri::command]
+fn create_page(vault_path:String, title:String) -> Result<String,String>
+{
+    let pages_dir = Path::new(&vault_path).join("pages");
+
+    let file_path = pages_dir.join(format!("{}.vkp", title));
+
+    if file_path.exists() 
+    {
+        return Err("Page already exists".into());
+    }
+
+    let page = serde_json::json!(
+    {
+        "id": Uuid::new_v4().to_string(),
+        "title":title,
+        "content": "<p></p>"
+    });
+
+    fs::write(file_path, serde_json::to_string_pretty(&page).unwrap())
+        .map_err(|e| e.to_string())?;
+
+    Ok("Page created".into())
+}
+
+#[tauri::command]
+fn list_pages(vault_path:String) -> Result<Vec<String>,String>
+{
+    let pages_dir = Path::new(&vault_path).join("pages");
+
+    let mut pages = vec![];
+
+    for entry in fs::read_dir(pages_dir).map_err(|e| e.to_string())? {
+        let entry = entry.map_err(|e| e.to_string())?;
+        let name = entry.file_name().to_string_lossy().to_string();
+        pages.push(name);
+    }
+
+    Ok(pages)
+}
+
+
+
+#[tauri::command]
+fn read_page(vault_path: String, file_name: String) -> Result<String, String> {
+    let file_path = Path::new(&vault_path)
+        .join("pages")
+        .join(file_name);
+
+    let content = fs::read_to_string(file_path)
+        .map_err(|e| e.to_string())?;
+
+    Ok(content)
+}
+
+
 
 #[tauri::command]
 fn open_vault(path: String) -> Result<String, String> {
@@ -53,7 +114,7 @@ fn create_vault(base_path: String, vault_name: String) -> Result<String, String>
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(tauri::generate_handler![create_vault, open_vault])
+        .invoke_handler(tauri::generate_handler![create_vault, open_vault, create_page, list_pages, read_page])
         .run(tauri::generate_context!())
         .expect("error running app");
 }
